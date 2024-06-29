56edd4acfcd3d07160436a86b223f09873d953d2
bekzod <qochqarovbekzod049@gmail.com>
2024-06-29 14:09:15 +0500

    postgres toliqq yozildi

--- storage/postgres/postgres.go
+++ storage/postgres/postgres.go
@@ -1,1 +1,32 @@
 package postgres
+
+import (
+	"database/sql"
+	"fmt"
+	_ "github.com/lib/pq"
+)
+
+const (
+	host     = "localhost"
+	port     = "5432"
+	user     = "postgres"
+	dbname   = "user_service"
+	password = "1918"
+)
+
+func ConnectDb() (*sql.DB, error) {
+
+	conn := fmt.Sprintf("host=%s port=%s user=%s dbname=%s password=%s sslmode=disable",
+		host, port, user, dbname, password)
+	db, err := sql.Open("postgres", conn)
+	if err != nil {
+		return nil, err
+	}
+
+	err = db.Ping()
+	if err != nil {
+		return nil, err
+	}
+
+	return db, nil
+}
