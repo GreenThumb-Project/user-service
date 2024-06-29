e27b90ef1bb37baef06f884b3877312ba9f0c037
bekzod <qochqarovbekzod049@gmail.com>
2024-06-29 15:07:18 +0500

    user crud funcsiyalari yozildi

--- storage/postgres/users.go
+++ storage/postgres/users.go
@@ -2,6 +2,7 @@ package postgres
 
 import (
 	"database/sql"
+	"time"
 	pb "user_service/generated/users"
 )
 
@@ -13,22 +14,58 @@ func NewUserRepo(db *sql.DB) *UserRepo {
 	return &UserRepo{DB: db}
 }
 
-func (u *UserRepo) GetUserById(id string) (*pb.GetUserByIdResponce, error) {
+func (u *UserRepo) GetUserById(id string) (*pb.CreateUsersResponce, error) {
+
 	return nil, nil
 }
+
+//------------------------------------------------------------------------------
 
 func (u *UserRepo) CreateUser(user *pb.CreateUsersRequest) (*pb.CreateUsersResponce, error) {
-	return nil, nil
+	var users *pb.CreateUsersResponce
+	err := u.DB.QueryRow(`
+			INSERT INTO 
+			users(
+				id,
+				username,
+				email,
+				password_hash
+			VALUES($1,$2,$3))
+		`, user.UserId, user.UserName, user.Email).Scan(&users)
+
+	return &pb.CreateUsersResponce{UserId: users.UserId,
+		UserName: users.UserName,
+		Email:    users.Email}, err
 }
+
+//-------------------------------------------------------------------------------------------
 
 func (u *UserRepo) DeleteUser(id string) (*pb.DeleteUserResponce, error) {
-	return nil, nil
+
+	_, err := u.DB.Exec(`
+			UODATE TABLE 
+			users 
+			SET
+				daleted_at=$1 
+				where 
+					id=$2
+			`, time.Now(), id)
+	if err != nil {
+		return &pb.DeleteUserResponce{Success: false}, err
+	}
+
+	return &pb.DeleteUserResponce{Success: true}, nil
 }
+
+//------------------------------------------------------------------------------------------
 
 func (u *UserRepo) GetUserByIdProfile(id string) (*pb.GetUserByIdProfileResponces, error) {
+
 	return nil, nil
 }
 
-func (u *UserRepo) UpdateUserProfile(id string) (*pb.UpdateUserProfileResponces, error) {
+//---------------------------------------------------------------------------------------------
+
+func (u *UserRepo) UpdateUserProfile(userProfil *pb.UpdateUserProfileRequest) (*pb.UpdateUserProfileResponces, error) {
 	return nil, nil
 }
