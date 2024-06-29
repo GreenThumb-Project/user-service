0d03c612fbb829139c2ff58390748687589882fd
Diyorbek Ne'matov <diyorbeknematov0321@gmail.com>
2024-06-29 14:02:28 +0500

    Postgres uchun CRUDlar yaratildi

--- storage/postgres/users.go
+++ storage/postgres/users.go
@@ -1,1 +1,19 @@
 package postgres
+
+import (
+	"database/sql"
+	u "user_service/generated/users"
+)
+
+type UserRepo struct {
+	DB *sql.DB
+}
+
+func NewUserRepo(db *sql.DB) *UserRepo {
+	return &UserRepo{DB: db}
+}
+
+func (u *UserRepo) GetUserById(id string) (*u.CreateUsersResponce, error) {
+
+	return nil, nil
+}	
